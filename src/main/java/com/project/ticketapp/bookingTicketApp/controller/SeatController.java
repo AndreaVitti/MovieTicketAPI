9119d36fb0f4ff9dbc9423638c24ee45b93500ac
AndreaVitti <av.andreavitti@gmail.com>
2025-05-04 19:12:56 +0200

    Fixed annotations

--- src/main/java/com/project/ticketapp/bookingTicketApp/controller/SeatController.java
+++ src/main/java/com/project/ticketapp/bookingTicketApp/controller/SeatController.java
@@ -5,13 +5,9 @@ import com.project.ticketapp.bookingTicketApp.service.SeatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/seats")
 public class SeatController {
